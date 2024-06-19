cc52cd3498a2a989237b6f13c57d580e6757a73f
Arshia <arshia@Arshias-MacBook-Air.local>
2024-06-20 02:37:19 +0330

    Update bot with main functionalities

--- bot.py
+++ bot.py
@@ -1,22 +1,53 @@
-from telegram import Update
+from telegram import Update, Bot, ParseMode
 from telegram.ext import Updater, CommandHandler, CallbackContext
 
-# Your bot token
 TOKEN = '7211395396:AAGNhfMUDSJdRlOB5DKoH-tjvyWuBotgM60'
 
-
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('سلام!')
+    update.message.reply_text(
+        "سلام! به بات من خوش آمدید. "
+        "از دستورات زیر می‌توانید استفاده کنید:\n"
+        "/info - مشخصات فردی\n"
+        "/contact - لینک های ارتباطی\n"
+        "/resume - رزومه"
+    )
 
-def main():
-    updater = Updater(TOKEN, use_context=True)
-    dp = updater.dispatcher
+def info(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(
+        "مشخصات فردی:\n"
+        "عرشیا افشار\n"
+        "متولد ۱۳۸۱/۰۱/۲۴\n"
+        "ساکن شهر: همدان / اصفهان\n"
+        "تحصیلات: کارشناسی برق (گرایش بیوالکتریک)"
+    )
 
-    dp.add_handler(CommandHandler("start", start))
+def contact(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(
+        "لینک های ارتباطی:\n"
+        "Gmail: arshiaafshar7@gmail.com\n"
+        "Telegram: @asshiiv\n"
+        "Instagram: @asshiiv\n"
+        "Phone Number: 09185842903"
+    )
+
+def resume(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(
+        "رزومه:\n"
+        "به زودی پیوست داده میشود"
+    )
+
+def main() -> None:
+    updater = Updater(TOKEN)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("info", info))
+    dispatcher.add_handler(CommandHandler("contact", contact))
+    dispatcher.add_handler(CommandHandler("resume", resume))
 
     updater.start_polling()
     updater.idle()
 
 if __name__ == '__main__':
     main()
-
